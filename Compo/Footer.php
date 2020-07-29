e8366e92705e4642fb99283e56ab4411d987fb8e
Harsh Bindal <harshbin06@gmail.com>
2020-07-29 19:24:01 +0530

    Starting Framework

---
+++ Compo/Footer.php
@@ -0,0 +1,8 @@
+
+<div class="container-fluid bg-dark text-light " style= " box-sizing: border-box">
+<!-- <div class="container-fluid bg-dark text-light" > -->
+<p class="text-center mb-0 " style= "margin-top:35rem ">
+Copyright Forum exclusively for students | All rights reserved 2020-2021
+
+</p>
+</div>
